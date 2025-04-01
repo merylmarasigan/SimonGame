a9705c80d6326c9aa88145a48a13316f3fb56f65
Meryl Marasigan <meryl0729@gmail.com>
2025-04-01 16:10:24 -0700

    Added sounds and animations to buttons clicked by player

--- game.js
+++ game.js
@@ -1,5 +1,22 @@
 let buttonColors = ['red','blue','green','yellow'];
 let gamePattern = [];
+let userClickedPattern = [];
+
+function playSound(btn){
+    let audio = new Audio('./sounds/'+btn+'.mp3');
+    audio.play();
+}
+
+function animatePress(currentColor){
+    $('#'+currentColor).addClass('pressed');
+
+    setTimeout(() => {
+        $('#'+currentColor).removeClass('pressed');
+        }, 100
+    );
+
+}
+
 function nextSequence(){
     //generate a random number between 0 and 3 
     let randomNumber = Math.floor(Math.random() * (3-0+1))
@@ -7,9 +24,14 @@ function nextSequence(){
     gamePattern.push(randomChosenColor);
 
     $('#'+randomChosenColor).animate({opacity: 0.3}).animate({opacity:1});
-    let audio = new Audio('./sounds/'+randomChosenColor+'.mp3');
-    audio.play();
+    playSound(randomChosenColor);
     
 }
 
-document.addEventListener('keydown', nextSequence);
+$('.btn').click((event) => {
+    let userChosenColor =  $(event.target).attr('id')
+    userClickedPattern.push(userChosenColor);
+    animatePress(userChosenColor);
+    playSound(userChosenColor);
+
+})
